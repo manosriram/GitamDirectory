076563dd91e88ec5e91472863c93cb4d7a89ac07
Mano Sriram <mano.sriram0@gmail.com>
2019-02-28 02:16:11 +0530

    modified gitignore.

--- setup/url.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURL:
-    "mongodb://manosriram:manomano1234@gitamdirectory-shard-00-00-ulrpt.mongodb.net:27017,gitamdirectory-shard-00-01-ulrpt.mongodb.net:27017,gitamdirectory-shard-00-02-ulrpt.mongodb.net:27017/test?ssl=true&replicaSet=GitamDirectory-shard-0&authSource=admin&retryWrites=true",
-  secret: "manomano1234"
-};
